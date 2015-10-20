f50c0ab3f42ee6cd9c861e57e649f7d9fdeddf5c
zakir <zakir.elahi@bankbazaar.com>
2015-10-20 12:18:02 +0530

    temporary repository

---
+++ app.js
@@ -0,0 +1,152 @@
+/// <reference path="typings/node/node.d.ts"/>
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongo = require('mongodb').MongoClient;
+
+var usernames = {};
+var userCount = 0;
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var mongo_url = process.envCUSTOMCONNSTR_MONGOLAB_URI || "mongodb://localhost:27017/exampleDb";
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+
+  var addedUser = false;
+  // Handle an 'add user' event
+  socket.on('add user', function(msg){
+    var usernameErr = isValidUsername(msg.username);
+
+
+
+      socket.username = msg.username;
+      usernames[msg.username] = socket.id;
+      userCount++;
+      // Tell the user they have successfully logged in
+      socket.emit('login', {
+        "userCount" : userCount,
+        "usernames": usernames
+      });
+      addedUser = true;
+      console.log('User "' + msg.username + '" connected. Current user count: ' +
+          userCount);
+      // Broadcast that a user has joined
+      socket.broadcast.emit('user joined', {
+        "username": msg.username,
+        "userCount": userCount,
+        "usernames": usernames
+      });
+      mongo.connect(mongo_url, function(err, db){
+        var collection = db.collection('chat_messages');
+        var stream = collection.find().sort({
+          _id: -1
+        }).limit(10).stream();
+
+        stream.on('data', function(chat){
+          socket.emit('chat history', chat);
+        });
+      });
+
+  });
+
+  socket.on('chat message', function(msg){
+    mongo.connect(mongo_url, function (err, db) {
+      var collection = db.collection('chat_messages');
+      var content = {
+        "username": socket.username,
+        "message": msg.message
+      };
+      collection.insert(content, function (err, o) {
+        if (err) {
+          console.warn(err.message);
+        }
+        else {
+          console.log("chat message inserted into db: " + msg);
+        }
+      });
+    });
+    socket.broadcast.emit('chat message', {
+      "username": socket.username,
+      "message": msg.message
+    });
+  });
+
+
+  // Handle a 'typing' event
+  socket.on('user typing', function(msg){
+    console.log(msg.username + ' is typing something...');
+    socket.broadcast.emit('user typing', {
+      "username": msg.username
+    });
+  });
+  socket.on('user stopped typing', function(msg){
+    console.log(msg.username + ' stopped typing.');
+    socket.broadcast.emit('user stopped typing', {
+      "username": msg.username
+    });
+  });
+
+  // Handle a 'private message' event
+  socket.on('private message', function(msg){
+    if(usernames[msg.to]){
+      io.to(usernames[msg.to]).emit('private message', {
+        "username": socket.username,
+        "message": msg.message
+      });
+    }
+  });
+  // Handle a 'disconnect' event
+  socket.on('disconnect', function(){
+    if(addedUser){
+      var username = socket.username;
+      delete usernames[socket.username];
+      userCount--;
+      // Broadcast that a user disconnected
+      socket.broadcast.emit('user disconnect', {
+        "user": username,
+        "userCount": userCount,
+        "usernames": usernames
+      });
+
+    }
+  });
+});
+
+http.listen(server_port, server_ip_address, function(){
+  console.log("Server listening on *:" + server_port);
+});
+
+var broadcastError = function(socket, errMsg){
+  socket.broadcast.emit('error', {
+    "message": errMsg
+  });
+};
+
+var emitError = function(socket, errType, errMsg){
+  socket.emit(errType, {
+    "message": errMsg
+  });
+};
+
+var isValidUsername = function(username){
+  var error = "";
+  var illegalChars = /\W/;
+
+  if(illegalChars.test(username)){
+    error = "Username must contain only aplhanumeric characters";
+  }
+  else if(username.length < 4 || username.length > 32){
+    error = "Username must be between 4 and 32 characters in length";
+  }
+
+  return error;
+};
---
+++ public/chat1.js
@@ -0,0 +1,153 @@
+var chat = chat || {};
+var x;
+jQuery.fn.reverse = [].reverse;
+
+
+(function($, window, chat, undefined){
+    var socket = io();
+    var username = '';
+    var connected = false;
+    var isTyping = false;
+    var typingTimer;
+    var typingInterval = 4000;
+    var usersTyping = {};
+
+    var config = {
+        chatPage: ".outerPanel",
+        loginPage: ".page-login",
+        loginError: ".login-error",
+        messages: ".messages",
+        userCount: ".page-chat .userCount",
+        userTyping: ".page-chat .userTyping",
+        loginForm: ".form-login",
+        msgForm: ".form-msg",
+        msgContainer: ".container-msg",
+        connectedUsers: ".connectedUsers"
+    };
+    var $chatPage, $loginPage, $messages, $userCount, $loginForm, $msgForm, $userTyping,
+        $msgContainer, $connectedUsers, $loginError;
+
+    chat.init = function(cfg){
+        $.extend(config, cfg);
+        // Cache some of the elements
+        $chatPage = $(config.chatPage);
+        $loginPage = $(config.loginPage);
+        $messages = $(config.messages);
+        $userCount = $(config.userCount);
+        $loginForm = $(config.loginForm);
+        $msgForm = $(config.msgForm);
+        $userTyping = $(config.userTyping);
+        $msgContainer = $(config.msgContainer);
+        $connectedUsers = $(config.connectedUsers);
+        $loginError = $(config.loginError);
+        setUpForms();
+    };
+
+    var setUpForms = function() {
+        $loginForm.submit(function () {
+            username = $(this).find('input.username').first().val().trim();
+            $loginError.text('');
+            if (username) {
+                socket.emit('add user', {
+                    "username": username
+                });
+            }
+            return false;
+        });
+
+
+        $msgForm.submit(function () {
+            var $msgInput = $(this).find('input.msg').first();
+            var msg = $msgInput.val();
+            var tokens = msg.split(' ');
+            switch (tokens[0]) {
+                case "@":
+                    if (tokens[1] && tokens[2]) {
+                        var message = msg.substring(tokens[0].length + tokens[1].length + 2);
+                        showPrivateMessage("Me to " + tokens[1], message);
+                        socket.emit('private message', {
+                            "username": username,
+                            "to": tokens[1],
+                            "message": message
+                        });
+                    }
+                    break;
+                default:
+                    if (msg != '') {
+                        showMyMessage(msg);
+                        socket.emit('chat message',
+                            {
+                                "message": msg
+                            });
+                    }
+                    break;
+            }
+
+            $msgInput.val('');
+            return false;
+        });
+
+    }
+    socket.on('login', function(data){
+        connected = true;
+        $loginPage.fadeOut();
+        $chatPage.show();
+        $loginPage.off('click');
+        showSystemMessage("Oh hey, welcome to Just Chattin'");
+        //updateUserCount(data.userCount);
+        //updateConnectedUsers(data.usernames);
+    });
+
+    socket.on('user joined', function(data){
+        showSystemMessage(data.username + ' has joined the chat');
+        updateUserCount(data.userCount);
+        updateConnectedUsers(data.usernames);
+    });
+
+    var showSystemMessage = function(message){
+        $messages.append($('<li>')
+            .addClass('text-muted')
+            .text(message));
+    };
+
+    var updateUserCount = function(userCount){
+        $userCount.text(userCount);
+    };
+    var updateConnectedUsers = function(usernames){
+        $connectedUsers.empty();
+            $.each(usernames, function(key, value){
+            var typing = $('<span>').addClass('glyphicon glyphicon-pencil typing').hide();
+            var user = $('<li>').prop('id', key).append(key).append(typing);
+            $connectedUsers.append(user);
+        });
+    };
+    var showMyMessage = function(message){
+        $messages.append($('<li>')
+            .addClass('text-primary')
+            .text("Me: " + message));
+        $msgContainer.scrollTop($msgContainer.height());
+    };
+
+    socket.on('chat message', function(data){
+        showMessage(data.username, data.message);
+    });
+
+    var showMessage = function(username, message){
+        $messages.append($('<li>')
+            .addClass('')
+            .text(username + ": " + message));
+        $msgContainer.scrollTop($msgContainer.height());
+    };
+
+    socket.on('private message', function(data){
+        showPrivateMessage(data.username, data.message);
+    });
+    var showPrivateMessage = function(username, message){
+        $messages.append($('<li>')
+            .addClass('text-warning')
+            .text(username + ": " + message));
+        $msgContainer.scrollTop($msgContainer.height());
+    };
+
+
+})(jQuery, window, chat)
